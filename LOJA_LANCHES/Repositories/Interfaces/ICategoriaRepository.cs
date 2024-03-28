c9d29f2df36afe4ae9b993ff0c5ccafaaad7fce1
João Meirinho <jao46roberto@gmail.com>
2024-03-27 21:27:58 -0300

    Implement repository for Categoria

---
+++ LOJA_LANCHES/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using LojaLanches.Models;
+
+namespace LojaLanches.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
