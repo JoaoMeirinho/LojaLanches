9c7ebbe2a31b903d83aed785abba7a35861903c8
João Meirinho <jao46roberto@gmail.com>
2024-04-01 21:00:53 -0300

    Implementado repository Lanche

---
+++ LOJA_LANCHES/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using LojaLanches.Models;
+
+namespace LojaLanches.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches();
+        IEnumerable<Lanche> LanchesPreferidos();
+        Lanche GetLancheById(int lancheId);
+    }
+}
---
+++ LOJA_LANCHES/Repositories/LancheRepository.cs
@@ -0,0 +1,27 @@
+﻿using LojaLanches.Context;
+using LojaLanches.Models;
+using LojaLanches.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaLanches.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+        public LancheRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public Lanche GetLancheById(int lancheId)
+        {
+            return _context.Lanches.FirstOrDefault(l =>  l.LancheId == lancheId);
+        }
+
+        public IEnumerable<Lanche> Lanches() => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos() => _context.Lanches
+            .Where(l => l.IsLanchePreferido)
+            .Include(c => c.Categoria);
+    }
+}
